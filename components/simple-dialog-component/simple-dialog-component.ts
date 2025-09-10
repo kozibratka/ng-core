0c52b6b4d98bc9b399cb5fe7294c6d1eb7d4b589
Martin Kozibrátka <martin.kozibratka@starnet.cz>
2025-09-10 12:17:18 +0200

    Update

---
+++ components/simple-dialog-component/simple-dialog-component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog';
+
+@Component({
+  selector: 'app-simple-dialog-component',
+  imports: [],
+  templateUrl: './simple-dialog-component.html',
+  styleUrl: './simple-dialog-component.scss'
+})
+export class SimpleDialogComponent {
+    text = '';
+
+    constructor(
+        public dynamicDialogConfig: DynamicDialogConfig,
+        public dynamicDialogRef: DynamicDialogRef,
+    ) {
+        this.text = this.dynamicDialogConfig.data.text;
+    }
+}
