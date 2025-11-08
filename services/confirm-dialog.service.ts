3f7d2729c80640b6d1aeed280bd23c999c4a090d
Martin Kozibrátka <martin.kozibratka@starnet.cz>
2025-11-08 19:23:12 +0100

    Update

--- services/confirm-dialog.service.ts
+++ services/confirm-dialog.service.ts
@@ -4,7 +4,7 @@ import {ConfirmationService} from "primeng/api";
 @Injectable({
   providedIn: 'root'
 })
-export class ConfirmDialogServiceService {
+export class ConfirmDialogService {
 
   constructor(
     private confirmationService: ConfirmationService
